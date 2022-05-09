ec9e37ec12b8107e3cf3fa11d1ad57096e803532
tomealbuquerque <tome.m.albuquerque@inesctec.pt>
2022-05-09 22:10:42 +0100

    test the model

---
+++ clean_dirty_cars_classifier/test.py
@@ -0,0 +1,37 @@
+import torch
+from PIL import Image
+from torchvision import transforms
+import torch.nn.functional as F
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def test_classifier(image_name="3efcdc3507.jpg"):
+    
+    im = Image.open(image_name)
+    
+    newsize = (224, 224)
+    im = im.resize(newsize)
+    
+    test_transforms = transforms.Compose([
+        
+        transforms.ToTensor(), 
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+    ])
+    
+    X = test_transforms(im)
+    
+    model = torch.load('baseline.pth')
+    phat = F.softmax(model(torch.unsqueeze(X, 0).to(device)),1)
+    
+    y_pred = phat.argmax(1)
+    y_pred = y_pred.cpu().numpy()
+    
+    if y_pred==0:
+        y_pred_='clean'
+    else:
+        y_pred_='dirty'
+    return print(y_pred_)
+    
+
+test_classifier()
