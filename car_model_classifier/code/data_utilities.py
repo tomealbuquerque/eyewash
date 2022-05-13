072b381d27eec8d975bde7cb4747932d3034eac9
Tiago Filipe Sousa Gonçalves <tiago.f.goncalves@inesctec.pt>
2022-05-13 12:15:46 +0100

    [car_model_classifier] Added training code

--- car_model_classifier/code/data_utilities.py
+++ car_model_classifier/code/data_utilities.py
@@ -13,9 +13,32 @@ import torchvision
 
 
 
+# Function: Resize images
+def resize_images(datapath, newpath, newheight=512):
+    
+    # Create new directories (if necessary)
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+    
+
+    # Go through data directory and generate new (resized) images
+    for f in tqdm(os.listdir(datapath)):
+        if(f.endswith(".jpg") or f.endswith('.png')):
+            img = Image.open(os.path.join(datapath, f))
+            w, h = img.size
+            ratio = w / h
+            new_w = int(np.ceil(newheight * ratio))
+            new_img = img.resize((new_w, newheight), Image.ANTIALIAS)
+            new_img.save(os.path.join(newpath, f))
+
+
+    return
+
+
+
 # StanfordCarsDataset: Dataset Class
 class StanfordCarsDataset(Dataset):
-    def __init__(self, base_data_path, data_split, transform=None):
+    def __init__(self, base_data_path, data_split, resized=False, transform=None):
         """
         Args:
             base_data_path (string): Data directory.
@@ -35,12 +58,22 @@ class StanfordCarsDataset(Dataset):
         
         # Train Split Variables
         if self.data_split.lower() == "train".lower():
+            if resized:
+                self.subset_images = os.path.join(self.base_images_path, "resized", "cars_train")
+
+            else:
+                self.subset_images = os.path.join(self.base_images_path, "cars_train")
+            
             self.annotations_mat_path = os.path.join(self.base_labels_path, "devkit", "cars_train_annos.mat")
-            self.subset_images = os.path.join(self.base_images_path, "cars_train")
         
         else:
+            if resized:
+                self.subset_images = os.path.join(self.base_images_path, "resized", "cars_test")
+                
+            else:
+                self.subset_images = os.path.join(self.base_images_path, "cars_test")
+            
             self.annotations_mat_path = os.path.join(self.base_raw_path, "cars_test_annos_withlabels.mat")
-            self.subset_images = os.path.join(self.base_images_path, "cars_test")
 
 
         # Get images and respective labels
@@ -84,13 +117,23 @@ class StanfordCarsDataset(Dataset):
 
 # To run and test the code
 if __name__ == "__main__":
-    for data_split in ["train", "test"]:
-        transforms = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
-        dataset = StanfordCarsDataset(base_data_path="data", data_split=data_split, transform=transforms)
-        dataloader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=4)
-        
+    resize_data = True
 
-        with torch.no_grad():
-            for images, labels in tqdm(dataloader):
-                print(f"Data Split: {data_split}")
-                print(f"Image Shape: {images.shape} | Label: {labels.item()}")
+    if resize_data:
+        for subset_folder in ["cars_test", "cars_train"]:
+            datapath = os.path.join("data", "images", subset_folder)
+            new_datapath = os.path.join("data", "images", "resized", subset_folder)
+
+            resize_images(datapath=datapath, newpath=new_datapath)
+
+    else:
+        for data_split in ["train", "test"]:
+            transforms = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
+            dataset = StanfordCarsDataset(base_data_path="data", data_split=data_split, transform=transforms)
+            dataloader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=4)
+            
+
+            with torch.no_grad():
+                for images, labels in tqdm(dataloader):
+                    print(f"Data Split: {data_split}")
+                    print(f"Image Shape: {images.shape} | Label: {labels.item()}")
